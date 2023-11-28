12e6ee00b54c9785a90bfdf21564c3f3e6c49f21
tn334 <102494845+tn334@users.noreply.github.com>
2023-11-27 23:41:51 -0700

    Updating tests in program

--- qaProject/deepstateTests.cpp
+++ qaProject/deepstateTests.cpp
@@ -10,27 +10,36 @@ using namespace deepstate;
 TEST(QAProject, SequentialVsParallel) {
     int N = DeepState_IntInRange(parameterStart, parameterEnd);
     int M = DeepState_IntInRange(parameterStart, parameterEnd);
-    double tolerance = .0005;
+    //double tolerance = DeepState_Double(timeDiffThreshold);
     double seqTime = 0, paraTime = 0;
     const char* filename = "output.txt";
+
+    ASSERT_GE(N, 0); // Assert that N is greater than or equal to 0
+
+    ASSERT_LT(loseRate, 1.0); // Assert that loseRate is less than 1.0
+
+    ASSERT_EQ(totalRuns, (parameterEnd / incrementor) * (parameterEnd / incrementor));
+
+
     // Run the sequential implementation
     double seqResult = sequentialRun(N, M);
 
     // Run the parallel implementation
     double paraResult = parrallelOptimizedRun(N, M);
 
-    seqTimer(N, M, seqTime); 
-    paraTimer(N, M, paraTime);
-
-    // Check if the results are equal within a tolerance
     ASSERT_EQ(seqResult, paraResult) << "Sequential and parallel results should be equal.";
 
-    ASSERT_TRUE(access(filename, F_OK) == 0) << "File " << filename << " not created";
+
+
+    // Check if the results are equal within a tolerance
+
 
     // Add more assertions if needed
     ASSERT_LT(seqTime, paraTime) << "Sequential time should be less than parallel time.";
+    seqTimer(N, M, seqTime);
+    paraTimer(N, M, paraTime);
 
-    //ASSERT_NEAR(expectedValue, actualValue, tolerance) << "Values are not within tolerance.";
+    ASSERT_NEAR(seqTime, paraTime, timeDiffThreshold) << "Values are not within tolerance.";
 
     // You can also check specific conditions based on your implementation
     // For example, you might want to check that the sequential implementation is faster
