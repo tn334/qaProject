a400df99795d9c2731826c08be99464fac998f82
tn334 <102494845+tn334@users.noreply.github.com>
2023-12-11 18:34:17 -0700

    Fixing assertions

--- qaProject/deepstateTests.cpp
+++ qaProject/deepstateTests.cpp
@@ -59,6 +59,6 @@ TEST(QAProject, SequentialVsParallel) {
     LOG(TRACE) << "Sequential Time = " << seqTime;
     LOG(TRACE) << "Parallelized Time = " << paraTime;
     // May make crashes
-    ASSERT_LT(seqTime, paraTime); // "Sequential time should be less than parallel time.";
-    ASSERT_FLOAT_NEAR(seqTime, paraTime, TIME_THRESHOLD); //Asserting that we are within .005
+    ASSERT_GT(seqTime, paraTime); // "Sequential time should be less than parallel time.";
+    ASSERT_FLOAT_NEAR(seqTime, paraTime, .005); //Asserting that we are within .005
 }
